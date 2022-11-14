218958cb494787e2b08ab7646dbc2b205efab018
Vladislav Bystritskiy <mrevlring@gmail.com>
2022-11-14 15:39:50 +0300

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // new Vector3(1, 0, 0)
+        transform.Translate(Vector3.right * Time.deltaTime);
+    }
+}
