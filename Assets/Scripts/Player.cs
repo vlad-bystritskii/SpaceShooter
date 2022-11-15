56afc72f8e47ca8fc257e0546362f96483527d7f
Vladislav Bystritskiy <mrevlring@gmail.com>
2022-11-15 16:20:58 +0300

    movement logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // new Vector3(1, 0, 0)
-        transform.Translate(Vector3.right * Time.deltaTime);
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
